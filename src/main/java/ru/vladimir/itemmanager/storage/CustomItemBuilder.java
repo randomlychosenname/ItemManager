a5ccb517fcd9aa374717e86af8f3c9bdf72a2ca9
Vladimir <vladimirkosilov361@gmail.com>
2026-06-28 13:04:42 +0400

    Custom item building caching is done, as well as item storage ready for file parsing implementation.

--- src/main/java/ru/vladimir/itemmanager/storage/CustomItemBuilder.java
+++ src/main/java/ru/vladimir/itemmanager/storage/CustomItemBuilder.java
@@ -1,7 +1,9 @@
 package ru.vladimir.itemmanager.storage;
 
+import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.inventory.ItemStack;
@@ -9,8 +11,9 @@ import org.jetbrains.annotations.NotNull;
 
 public final class CustomItemBuilder {
 
+    private static final long ITEM_CACHE_ENTRY_EXPIRE_IN = 180000L;
     private final CustomItemStorage itemStorage;
-    private final Map<String, ItemStack> itemCache;
+    private final Map<String, ItemCacheEntry> itemCache;
 
     public CustomItemBuilder(@NotNull CustomItemStorage itemStorage) {
         this.itemStorage = itemStorage;
@@ -18,12 +21,52 @@ public final class CustomItemBuilder {
     }
     
     public @NotNull Optional<ItemStack> build(@NotNull String itemId) {
+        clearOldCacheEntries();
+
         if (!itemStorage.isCustomItem(itemId))
             return Optional.empty();
 
         if (itemCache.containsKey(itemId))
-            return Optional.of(itemCache.get(itemId));
+            return Optional.of(itemCache.get(itemId).item);
 
-        return itemStorage.getCustomItem(itemId);
+        final Optional<ItemStack> customItem = itemStorage.getCustomItem(itemId);
+
+        if (customItem.isEmpty())
+            return Optional.empty();
+
+        addNewCacheEntry(itemId, customItem.get());
+
+        return customItem;
+    }
+
+    private void clearOldCacheEntries() {
+        if (itemCache.isEmpty()) return;
+
+        final Instant timeNow = Instant.now();
+
+        for (var entry : Set.copyOf(itemCache.entrySet())) {
+            if (entry.getValue().isExpired(timeNow)) {
+                itemCache.remove(entry.getKey());
+            }
+        }
+    }
+
+    private void addNewCacheEntry(String itemId, ItemStack item) {
+        itemCache.put(itemId, new ItemCacheEntry(item, Instant.now()));
+    }
+
+    private static class ItemCacheEntry {
+        
+        private final ItemStack item;
+        private final Instant timestamp;
+
+        private ItemCacheEntry(ItemStack item, Instant timestamp) {
+            this.item = item;
+            this.timestamp = timestamp;
+        }
+
+        private boolean isExpired(Instant newestTimestamp) {
+            return (newestTimestamp.getEpochSecond() - timestamp.getEpochSecond()) > ITEM_CACHE_ENTRY_EXPIRE_IN;
+        }
     }
 }
--- src/main/java/ru/vladimir/itemmanager/storage/CustomItemStorage.java
+++ src/main/java/ru/vladimir/itemmanager/storage/CustomItemStorage.java
@@ -1,9 +1,11 @@
 package ru.vladimir.itemmanager.storage;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -12,44 +14,80 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
 import ru.vladimir.itemmanager.ItemManager;
+import ru.vladimir.itemmanager.utils.Logger;
 
 public final class CustomItemStorage {
+
+    private final ItemManager plugin;
     private final Map<String, byte[]> itemRegistry;
 
     public CustomItemStorage(@NotNull ItemManager plugin) {
-        itemRegistry = readUserStorage(YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "items.yml")));
+        this.plugin = plugin;
+        this.itemRegistry = new ConcurrentHashMap<>();
+
+        refreshItemRegistry(getItemConfigFile(), getItemConfig());
     }
 
-    private Map<String, byte[]> readUserStorage(FileConfiguration config) {
-        // Do something.
-        return Map.of();
+    private void refreshItemRegistry(File file, FileConfiguration itemConfig) {
+        itemRegistry.clear(); 
+        
+        // Actual logic here.
     }
 
-    private void appendItemToUserStorage(String itemId, ItemStack item) {
+    private void appendItemToStorage(File file, FileConfiguration itemConfig, String itemId, ItemStack item) {
+        refreshItemRegistry(file, itemConfig);
 
+        // Here we append the item.
+
+        saveItemConfig(file, itemConfig);
+
+        refreshItemRegistry(file, itemConfig);
     }
 
-    private void removeItemFromUserStorage(String itemId) {
+    private void removeItemFromStorage(File file, FileConfiguration itemConfig, String itemId) {
+        refreshItemRegistry(file, itemConfig);
 
+        // Here we remove the item.
+
+        saveItemConfig(file, itemConfig);
+
+        refreshItemRegistry(file, itemConfig);
     }
 
-    private void writePluginStorage() {
+    private File getItemConfigFile() {
+        return new File(plugin.getDataFolder(), "items.yml");
+    }
 
+    private FileConfiguration getItemConfig() {
+        return YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "items.yml"));
+    }
+
+    private void saveItemConfig(File file, FileConfiguration config) {
+        if (!file.exists()) {
+            plugin.saveResource("items.yml", false);
+            Logger.debug(this, "items.yml was not found. A new version was created.");
+        }
+
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            Logger.error(this, "Failed to save file configuration.", e);
+        }
     }
 
     public boolean registerCustomItem(@NotNull String itemId, @NotNull ItemStack item) {
         if (isCustomItem(itemId)) return false;
-        appendItemToUserStorage(itemId, item);
-        itemRegistry.put(itemId, item.serializeAsBytes());
-        writePluginStorage();
+
+        appendItemToStorage(getItemConfigFile(), getItemConfig(), itemId, item);
+
         return true;
     }
 
     public boolean unregisterCustomItem(@NotNull String itemId) {
         if (!isCustomItem(itemId)) return false;
-        removeItemFromUserStorage(itemId);
-        itemRegistry.remove(itemId);
-        writePluginStorage();
+
+        removeItemFromStorage(getItemConfigFile(), getItemConfig(), itemId);
+        
         return true;
     }
 
