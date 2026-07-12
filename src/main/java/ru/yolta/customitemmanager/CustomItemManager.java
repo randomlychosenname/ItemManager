b3f00f2cfd601af279e9a7d5a68dd8596c443129
Wiore <285279589+wiore64@users.noreply.github.com>
2026-07-12 16:15:37 +0400

    Adjusted code style, removed formatting and removed the setting log level behavior
    Trying to improve the readability of the code, while addressing the slight performance issues. On top of that, changing log level was determined to be meaningless since it doesn't log much anyway. Only more logic to handle otherwise.

--- src/main/java/ru/yolta/customitemmanager/CustomItemManager.java
+++ src/main/java/ru/yolta/customitemmanager/CustomItemManager.java
@@ -1,6 +1,5 @@
 package ru.yolta.customitemmanager;
 
-import org.apache.logging.log4j.Level;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -15,38 +14,36 @@ import ru.yolta.customitemmanager.utils.Logger;
 import ru.yolta.customitemmanager.utils.Messenger;
 import ru.yolta.customitemmanager.utils.UpdateChecker;
 
-public class CustomItemManager extends JavaPlugin {
+public final class CustomItemManager extends JavaPlugin {
 
     public static final String PLUGIN_NAME = "CustomItemManager";
-    public static final String PLUGIN_PREFIX = "CIM";
     private static CustomItemManagerApi api;
     
     @Override
     public void onEnable() {
-        Logger.info(PLUGIN_NAME, "Loading up...");
+        Logger.info(PLUGIN_NAME, "Hello! Loading up...");
 
-        final ConfigProvider configProvider = new ConfigProvider(this);
+        final var configProvider = new ConfigProvider(this);
 
-        Logger.setLevel(configProvider.getGeneralConfig().loggingLevel());
         Messenger.setPrefix(configProvider.getMessageConfig().prefix());
 
-        final CustomItemStorage itemStorage = new CustomItemStorage(this);
-        final CustomItemBuilder itemBuilder = new CustomItemBuilder(itemStorage);
+        final var itemStorage = new CustomItemStorage(this);
+        final var itemBuilder = new CustomItemBuilder(itemStorage);
 
         api = new CustomItemManagerApi(this, itemStorage, itemBuilder);
 
-        final CommandService commandService = new CommandService(configProvider.getMessageConfig());
-        final CustomItemManagerCommand commandHandler = new CustomItemManagerCommand(commandService, configProvider.getMessageConfig());
+        final var commandService = new CommandService(configProvider.getMessageConfig());
+        final var commandHandler = new CustomItemManagerCommand(commandService, configProvider.getMessageConfig());
 
         final PluginCommand command = this.getCommand("customitemmanager");
-        if (command == null) throw new IllegalStateException("Command '%s' not found in plugin.yml".formatted("customitemmanager"));
+        if (command == null) throw new IllegalStateException("Failed to find command in 'plugin.yml': customitemmanager");
 
         command.setExecutor(commandHandler);
         command.setTabCompleter(commandHandler);
 
         checkUpdates();
 
-        Logger.info(PLUGIN_NAME, "Loaded successfully.");
+        Logger.info(PLUGIN_NAME, "Loaded successfully!");
     }
 
     private void checkUpdates() {
@@ -84,7 +81,7 @@ public class CustomItemManager extends JavaPlugin {
         Logger.info(PLUGIN_NAME, "Shutting down...");
 
         final PluginCommand command = this.getCommand("customitemmanager");
-        if (command == null) throw new IllegalStateException("Command '%s' not found in plugin.yml".formatted("customitemmanager"));
+        if (command == null) throw new IllegalStateException("Failed to find command in 'plugin.yml': customitemmanager");
 
         command.setExecutor(null);
         command.setTabCompleter(null);
@@ -93,9 +90,7 @@ public class CustomItemManager extends JavaPlugin {
 
         api = null;
 
-        Logger.setLevel(Level.INFO);
-
-        Logger.info(PLUGIN_NAME, "Shut down successfully.");
+        Logger.info(PLUGIN_NAME, "Shut down successfully. Bye!");
     }
 
     public static @NotNull CustomItemManagerApi getApi() {
