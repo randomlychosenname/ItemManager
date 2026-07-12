79b1d01ea78254889b2c9f68cb63de8799650481
Wiore <285279589+wiore64@users.noreply.github.com>
2026-07-12 16:20:10 +0400

    Removed general config from plugin
    Consequently, since log level is not being changed anymore, the only supported feature by the general config is no longer needed.
    
    Therefore, it was decided to completely erase the general config from code until it will be needed again.

--- src/main/java/ru/yolta/customitemmanager/config/ConfigProvider.java
+++ src/main/java/ru/yolta/customitemmanager/config/ConfigProvider.java
@@ -12,17 +12,11 @@ import ru.yolta.customitemmanager.utils.Logger;
 
 public final class ConfigProvider {
 
-    private static final String GENERAL_CONFIG_FILE_NAME = "config.yml";
     private static final String MESSAGE_CONFIG_FILE_NAME = "messages.yml";
-    private final GeneralConfig generalConfig;
     private final MessageConfig messageConfig;
 
     public ConfigProvider(@NotNull CustomItemManager plugin) {
         Logger.debug(this, "Initializing...");
-
-        final File generalConfigFile = new File(plugin.getDataFolder(), GENERAL_CONFIG_FILE_NAME);
-        ensureFileExists(plugin, generalConfigFile, false);
-        this.generalConfig = GeneralConfig.parseGeneralConfig(this, generalConfigFile, getFileConfig(generalConfigFile));
 
         final File messageConfigFile = new File(plugin.getDataFolder(), MESSAGE_CONFIG_FILE_NAME);
         ensureFileExists(plugin, messageConfigFile, false);
@@ -33,18 +27,6 @@ public final class ConfigProvider {
         Logger.debug(this, "Initialized successfully.");
     }
 
-    void ensureFileExists(@NotNull CustomItemManager plugin, @NotNull File file, boolean shouldReplace) {
-        if (!file.exists()) {
-            Logger.warn(this, "File '{}' not found. Creating it now.", file.getName());
-            plugin.saveResource(file.getName(), shouldReplace);
-            return;
-        }
-
-        if (shouldReplace) {
-            plugin.saveResource(file.getName(), true);
-        }
-    }
-
     void saveConfig(@NotNull File file, @NotNull FileConfiguration fileConfig) {
         try {
             fileConfig.save(file);
@@ -53,12 +35,19 @@ public final class ConfigProvider {
         }
     }
 
-    private FileConfiguration getFileConfig(File file) {
-        return YamlConfiguration.loadConfiguration(file);
+    void ensureFileExists(@NotNull CustomItemManager plugin, @NotNull File file, boolean shouldReplace) {
+        ensureFileExists(plugin, file, file.getName(), shouldReplace);
     }
 
-    public @NotNull GeneralConfig getGeneralConfig() {
-        return generalConfig;
+    private void ensureFileExists(@NotNull CustomItemManager plugin, @NotNull File file, @NotNull String path, boolean shouldReplace) {
+        if (!file.exists()) {
+            Logger.warn(this, "File '{}' not found. Creating it now.", file.getName());
+            plugin.saveResource(file.getName(), shouldReplace);
+        }
+    }
+
+    private FileConfiguration getFileConfig(File file) {
+        return YamlConfiguration.loadConfiguration(file);
     }
 
     public @NotNull MessageConfig getMessageConfig() {
